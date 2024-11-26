1a72e5dee4623e93acc61e5311e5aa7295f089dc
seyfu-t <git@seyfu-t.me>
2024-11-26 23:04:57 +0100

    added simple helper enums

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="astro/client" />
+
+// env.d.ts
+export enum ShiftType {
+    Nachtschicht = "N",
+    Tagschicht = "T",
+    Frei1 = "F1",
+    Frei2 = "F2"
+}
+
+export type ShiftColors = {
+    [key in ShiftType]: string;
+};
